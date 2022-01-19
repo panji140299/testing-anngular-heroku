40f66b1ab0931882e66a47f63fe75845241534be
panji140299 <panjiprasetya999@gmmail.com>
2022-01-19 11:49:43 +0700

    server

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
-function requireHTTPS(req, res, next){
+function requireHTTPS(req,res,next){
     if(!req.secure && req.get('x-forwarded-proto') !== 'https'){
-        return res.redirect('https://'+req.get('host')+req.url);
+      return res.redirect('https://'+req.get('host')+req.url)
     }
-    next();
-}
-
-const express = require('express');
-const app = express();
-
-app.use(requireHTTPS);
-app.unsubscribe(express.static('./dist/testing'));
-
-app.get('/*', (req,res)=> 
-    res.sendFile('index.html', {root: 'dist/testing/'}),
-);
-
-const port = process.env.PORT || 8080
-app.listen(port, ()=>{
+    next()
+  }
+  const express = require('express')
+  const app = express()
+  app.use(requireHTTPS)
+  app.use(express.static('./dist/testing'))
+  app.get('/*', (req,res)=>
+    res.sendFile('index.html',{root: 'dist/testing/'})
+  )
+  const port = process.env.PORT || 8080
+  app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-})
+  })
